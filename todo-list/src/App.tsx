57d3546176d8dceec9eb7e70159ca234cd7fed60
ramikh96 <ramikh.developer@gmail.com>
2020-10-09 22:55:12 +0200

    add TodoListItem component to App

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { TodoListItem } from './TodoListItem';
+
+const todos: Todo[] = [
+  {
+    text: 'Walk the dog',
+    complete: false,
+  },
+  {
+    text: 'Write app',
+    complete: true,
+  },
+];
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <ul>
+        <TodoListItem todo={todos[0]} />
+        <TodoListItem todo={todos[1]} />
+      </ul>
+    </div>
+  );
 }
 
 export default App;
